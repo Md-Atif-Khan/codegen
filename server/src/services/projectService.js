20e5d61079c3a42dd730ea51d06333aad9013672
Mohammed Atif Khan <mohammed.khan@oyorooms.com>
2025-07-19 17:34:00 +0530

    fixed createProject api

--- server/src/services/projectService.js
+++ server/src/services/projectService.js
@@ -1,21 +1,25 @@
 import Project from '../models/Project.js';
 import { NotFoundError, UnauthorizedError } from '../utils/errors.js';
+import User from '../models/User.js';
 
 export const getProjects = async (userId) => {
   return await Project.find({ user: userId });
 };
 
 export const createProject = async (userId, projectData) => {
+  const user = await User.findById(userId).select('-password');
+  if (!user) throw new Error('User not found');
   const project = new Project({
     name: projectData.name,
     description: projectData.description,
     classStructure: projectData.classStructure,
     code: projectData.code,
-    user: userId,
+    language: projectData.language, 
+    userId: user._id
   });
+
   return await project.save();
 };
-
 
 export const getProject = async (projectId, userId) => {
   const project = await Project.findOne({ _id: projectId, user: userId });
